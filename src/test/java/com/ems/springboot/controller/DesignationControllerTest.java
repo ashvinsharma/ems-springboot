faf51e4f33010b5c5a1c7c2e5b13ac9b301d53f5
Ashvin Sharma <ashvinsharma97@gmail.com>
2019-06-07 14:09:02 +0530

    Added some unit test for... well... testing

---
+++ src/test/java/com/ems/springboot/controller/DesignationControllerTest.java
@@ -0,0 +1,26 @@
+package com.ems.springboot.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DesignationControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getAllDesignations() throws Exception {
+        this.mockMvc.perform(get("/designation/read/all"))
+                .andExpect(status().isOk());
+    }
+}
