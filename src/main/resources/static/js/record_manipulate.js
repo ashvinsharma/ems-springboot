25d0fb7bd5a052af94cfb59c9709587fa8238b8b
Ashvin Sharma <ashvinsharma97@gmail.com>
2019-06-11 17:53:05 +0530

    parsing of designation on table completed
    ### TODO
    - editing cells
    - deleting cells

--- src/main/resources/static/js/record_manipulate.js
+++ src/main/resources/static/js/record_manipulate.js
@@ -3,40 +3,42 @@ let designation = {};
 $(document).ready(async () => {
     await $.get('/designation/read/all', data => {
         designation = data.content;
-        // put the data in the select options of all the entries in the table
-    }).then();
+    });
 
     const params = new URL(window.location.href).searchParams;
-    let url;
-    if (params.has('email')) {
-        url = `/employee/read/${params.get('email')}`;
-    } else {
-        url = '/employee/read/all';
-    }
-
-    $.get(url, data => {
-        const employee = data.content;
-        populate_table(employee);
-        // update table contents
-    });
+    $.get(params.has('email') ? `/employee/read/${params.get('email')}` : '/employee/read/all',
+        data => {
+            populate_table(data.content);
+        });
 
     $('#search-record').submit(event => {
         event.preventDefault();
         submit_search_query(event);
     });
 
-
+    $(document).on('click', '.update_button', function () {
+        const id = $(this).parent().closest('tr').attr('id');
+        const email = $(`#email_${id}`).trim();
+        const name = $(`#name_${id}`).trim();
+        const contact = $(`#contact_${id}`).trim();
+    });
 });
 
 const populate_table = employee => {
     $.each(employee, function () {
-        const $tr = $(`<tr id='row_${this.id}'>`).append(
+        const $select = $('<select></select>').attr('id', `designation_${this.id}`);
+        designation.map(d => {
+            $select.append($('<option />').val(d.id).text(d.name)
+                .attr('selected', (this.designationId.id === d.id)));
+        });
+        const $tr = $(`<tr id=${this.id}>`).append(
             $(`<th scope="row" id='id_${this.id}'>`).text(this.id),
             $(`<td id='email_${this.id}'>`).text(this.email).wrapInner('<div contenteditable>'),
             $(`<td id='name_${this.id}'>`).text(this.name).wrapInner('<div contenteditable>'),
+            $(`<td id='designationId_${this.id}'>`).append($select),
             $(`<td id='contact_${this.id}'>`).text(this.contact).wrapInner('<div contenteditable>'),
-            $(`<td id='action_${this.id}'>`).append(`<button type='button' id='update_${this.id}'>Update</button>` +
-                `<button type='button' id='delete_${this.id}'>Delete</button>`)
+            $(`<td id='action_${this.id}'>`).append(`<button type='button' id='update_${this.id}' class='update_button'>Update</button>` +
+                `<button type='button' id='delete_${this.id}' class='delete_button'>Delete</button>`)
         );
         $('#tbody-employees').append($tr);
     });
